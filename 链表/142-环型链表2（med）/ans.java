842111ae5ecc50518abd6b40c7e794dfd0d727c2
GoldenFlash668 <45288188+GoldenFlash668@users.noreply.github.com>
2020-10-10 22:43:44 +0800

    环型链表

---
+++ 链表/142-环型链表2（med）/ans.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                while(head != slow){
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return head;
+            }
+        }
+        return null;
+    }
+}
