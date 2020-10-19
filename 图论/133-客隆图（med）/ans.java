1e16d9529fc9b8f4668740c762dbe22d53e8a425
GoldenFlash668 <45288188+GoldenFlash668@users.noreply.github.com>
2020-10-19 13:56:19 +0800

    图论中等难度题

---
+++ 图论/133-客隆图（med）/ans.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> visited = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+        Node newnode = new Node(node.val);
+        visited.put(node, newnode);
+        for(Node curr:node.neighbors){
+            newnode.neighbors.add(cloneGraph(curr));
+        }
+        return newnode;
+    }
+}
