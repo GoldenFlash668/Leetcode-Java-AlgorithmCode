3c2624ce340b3fecd54bc7552dc647c53cc0371f
GoldenFlash668 <45288188+GoldenFlash668@users.noreply.github.com>
2020-10-23 13:03:41 +0800

    树的递归调用（bfs）

---
+++ 树/129-求根到叶子节点数字之和(Med)/ans.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+    public int dfs(TreeNode root, int sum){
+        if(root == null){
+            return 0;
+        }
+        sum = sum *10 + root.val;
+        if(root.left == null && root.right == null){
+            return sum;
+        }
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+}
